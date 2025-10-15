3125ac9ce6f8a202185a97f9761d024a4e4a1223
Isindil <nashry@sheridancollege.ca>
2025-10-15 14:59:20 -0400

    Added discount feature by Ryan

--- ProductManager.java
+++ ProductManager.java
@@ -1,5 +1,11 @@
 package ProductManager;
 
 public class ProductManager {
-    
+     public static void main(String[] args) {
+        System.out.println("Hello, Product Manager!");
+    }
+
+    public static double calculateDiscount(double price, double percent) {
+        return price - (price * (percent / 100));
+    }
 }
